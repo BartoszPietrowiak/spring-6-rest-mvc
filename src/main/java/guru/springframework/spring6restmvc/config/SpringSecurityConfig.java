93c58560c1cd8f5eceac6cc82cc98ac3dcb30605
bpietrowiak <bpietrowiak@consdata.pl>
2023-12-02 14:49:51 +0100

    Rest assured request validation

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -2,9 +2,11 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
@@ -12,6 +14,8 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests()
+                .requestMatchers("/v3/api-docs**", "/swagger-ui/**,", "/swagger-ui.html")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
